cc0208c1cad3606c854bb9fd3be1686b3e6dfb7e
Mehmet Gani TOMBALAK <mgani.tombalak@yahoo.com>
2018-12-21 17:40:53 +0300

    Packages update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, Event, BrowserWindowConstructorOptions } from 'electron'
+import { app, BrowserWindow, Event, BrowserWindowConstructorOptions, Menu } from 'electron'
 import * as path from 'path'
 import * as url from 'url'
 import { AppEvent } from './common/app-events';
@@ -6,11 +6,15 @@ import { stringify } from 'querystring';
 
 class Applicaton {
     static start(): void { new Applicaton() };
+
+    
     private readonly instance: Electron.App = app;
     private mainWindow: Promise<BrowserWindow>;
     constructor() {
         this.instance.on(AppEvent.WindowAllClosed, this.onAllWindowsClosed);
         this.instance.on(AppEvent.Ready, this.onReady);
+
+        
     }
 
     private onReady(): void {
@@ -47,6 +51,84 @@ class Applicaton {
     private onWindowClosed(): void {
         this.mainWindow = null;
         console.log('Window has been closed.');
+    }
+
+    private prepareMenu():void{
+        const template = [
+            {
+              label: 'Edit',
+              submenu: [
+                {role: 'undo'},
+                {role: 'redo'},
+                {type: 'separator'},
+                {role: 'cut'},
+                {role: 'copy'},
+                {role: 'paste'},
+                {role: 'pasteandmatchstyle'},
+                {role: 'delete'},
+                {role: 'selectall'}
+              ]
+            },
+            {
+              label: 'View',
+              submenu: [
+                {role: 'reload'},
+                {role: 'forcereload'},
+                {role: 'toggledevtools'},
+                {type: 'separator'},
+                {role: 'resetzoom'},
+                {role: 'zoomin'},
+                {role: 'zoomout'},
+                {type: 'separator'},
+                {role: 'togglefullscreen'}
+              ]
+            },
+            {
+              role: 'window',
+              submenu: [
+                {role: 'minimize'},
+                {role: 'close'}
+              ]
+            },
+            {
+              role: 'help',
+              submenu: [
+                {
+                  label: 'Learn More',
+                  click () { require('electron').shell.openExternal('https://electronjs.org') }
+                }
+              ]
+            }
+          ]
+          
+          if (process.platform === 'darwin') {
+            template.unshift({
+              label: app.getName(),
+              submenu: [
+                {role: 'about'},
+                {type: 'separator'},
+                {role: 'services'},
+                {type: 'separator'},
+                {role: 'hide'},
+                {role: 'hideothers'},
+                {role: 'unhide'},
+                {type: 'separator'},
+                {role: 'quit'}
+              ]
+            })
+          
+            // Window menu
+            template[3].submenu = [
+              {role: 'close'},
+              {role: 'minimize'},
+              {role: 'zoom'},
+              {type: 'separator'},
+              {role: 'front'}
+            ]
+          };
+          
+          const menu = Menu.buildFromTemplate(template)
+          Menu.setApplicationMenu(menu)
     }
 }
 Applicaton.start();
